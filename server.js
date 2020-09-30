34577aac9c755775d73374fe8fa3b80b1fa9b8e6
mmitch2958 <mikescottmitchell@outlook.com>
2020-09-30 13:20:04 -0400

    added passCheck func

--- server.js
+++ server.js
@@ -6,20 +6,33 @@ const hubsRouter = require('./hubs/hubs-router.js');
 const server = express();
 
 const logger = morgan('combined')
-//middleware 
+//middleware
+function greeter(req, res, next) {
+  console.log('hello')
+  next();
+} 
+//for any other password , or none, respond with http status 401 message
+function passCheck(req, res, next) {
+  if (req.headers.password === "mellon") {
+    next();
+  } else {
+    res.status(401).json({ errorMessage: "Inccorect password" });
+  }
+}
+server.use(passCheck);
 server.use(express.json());
 server.use(logger); 
+server.use(greeter);
+
 
 //endpoints 
 server.use('/api/hubs', hubsRouter);
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+server.get("/", (req, res) => {
+  const password = req.headers.password;
+  const nameInsert = req.name ? ` ${req.name}` : "";
+  res.status(200).json({ name: nameInsert, password });
 
-  res.send(`
-    <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
-    `);
 });
 
 module.exports = server;
